017dfeaf3ae9709ae0a4d6d223233f59b3a8b431
Pherpher089 <christutor089@gmail.com>
2024-08-17 09:07:46 -0700

    Added some changes for perspective change

--- Assets/Character/Scripts/CameraController.cs
+++ Assets/Character/Scripts/CameraController.cs
@@ -8,10 +8,13 @@ public class CameraController : MonoBehaviour
     [HideInInspector] public static CameraController Instance;
     [Range(1, 5)] public float m_CameraSmooth = 1;
     [Range(1, 5)] public float m_CameraScaleModifier = 1;
+    public bool m_IsTopDown = true;
+
+    public Vector3[] m_CameraPositionsAndRotations;
 
     Camera m_MainCamera;
     float m_CameraBaseHeight;
-    
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -24,7 +27,23 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_MainCamera.transform.position = Vector3.Lerp(m_MainCamera.transform.position, new Vector3(m_MainCamera.transform.position.x,m_CameraBaseHeight + GrowthManager.Instance.m_GrowthLevel * m_CameraScaleModifier, m_MainCamera.transform.position.z), Time.deltaTime * m_CameraSmooth);
+        float zAxis = m_IsTopDown ? m_MainCamera.transform.position.z : m_MainCamera.transform.position.z - GrowthManager.Instance.m_GrowthLevel * m_CameraScaleModifier;
+        m_MainCamera.transform.position = Vector3.Lerp(m_MainCamera.transform.position, new Vector3(m_MainCamera.transform.position.x, m_CameraBaseHeight + GrowthManager.Instance.m_GrowthLevel * m_CameraScaleModifier, zAxis), Time.deltaTime * m_CameraSmooth);
         transform.position = Vector3.Lerp(transform.position, m_PlayerTransform.position, Time.deltaTime * m_CameraSmooth);
+        ChangePerspectives(m_IsTopDown);
+    }
+
+    void ChangePerspectives(bool isTopDown)
+    {
+        if (isTopDown)
+        {
+            m_MainCamera.transform.localPosition = Vector3.Lerp(m_MainCamera.transform.localPosition, m_CameraPositionsAndRotations[0], Time.deltaTime * m_CameraSmooth);
+            m_MainCamera.transform.rotation = Quaternion.Euler(Vector3.Lerp(m_MainCamera.transform.rotation.eulerAngles, m_CameraPositionsAndRotations[1], Time.deltaTime * m_CameraSmooth));
+        }
+        else
+        {
+            m_MainCamera.transform.localPosition = Vector3.Lerp(m_MainCamera.transform.localPosition, m_CameraPositionsAndRotations[2], Time.deltaTime * m_CameraSmooth);
+            m_MainCamera.transform.rotation = Quaternion.Euler(Vector3.Lerp(m_MainCamera.transform.rotation.eulerAngles, m_CameraPositionsAndRotations[3], Time.deltaTime * m_CameraSmooth));
+        }
     }
 }
